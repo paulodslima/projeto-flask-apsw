6da6f3d6ef9a458cbc8168467d62104aaaffe7d9
paulodslima <paulodslima@outlook.com>
2021-08-20 11:40:22 -0300

    Aula 11

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from config import db
+from usuario import Usuario
 
 TEMPLATE = './templates'
 
@@ -6,6 +8,14 @@ STATIC = './static'
 
 app = Flask(__name__, template_folder=TEMPLATE, static_folder=STATIC )
 
+# Configuraçã de banco de dados
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///./dados.db'
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+ 
+#Definição das rotas
 @app.route('/')
 def helloWorld():
     return 'hello World'
@@ -14,4 +24,16 @@ def helloWorld():
 def home():
     return render_template('home.html')
 
-##app.run(host='0.0.0.0', port=5000)
+@app.route('/cadastrarUsuario')
+def cadastrarUsuario():
+    usuario = Usuario('Flavio medeiros', 'flaviomedeiros@ifal.edu.br')
+    db.session.add(usuario)
+    db.session.commit()
+    return 'Usuario cadastrado com sucesso'
+
+@app.route('/consultarUsuarios')
+def consultarUsuarios():
+    usuarios = Usuario.query.all()
+    return render_template('mostrarUsuarios.hmtl', usuarios=usuarios)
+
+app.run(host='0.0.0.0', port=5000)
